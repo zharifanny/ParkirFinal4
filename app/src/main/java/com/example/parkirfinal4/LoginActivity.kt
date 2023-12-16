cf8f005b010b6c036aed92f259250bbc9fc9768b
Khindy Zharifany <zharifanny@gmail.com>
2023-12-16 23:20:46 +0700

    Fixed Register

--- app/src/main/java/com/example/parkirfinal4/LoginActivity.kt
+++ app/src/main/java/com/example/parkirfinal4/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.parkirfinal4
 import DatabaseHelper
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -41,5 +42,11 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
             }
         }
+
+
+    }
+    fun goToNext(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
